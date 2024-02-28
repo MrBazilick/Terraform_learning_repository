0603334fe446eef7b99efe70a58f673c044b8db1
MrBazilick <96795399+MrBazilick@users.noreply.github.com>
2024-02-28 02:03:05 +0200

    continue to work with secrets
    temporary commented secrets section

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
-# Secret creation module calling
-module "secret-creation" {
-  source = "../secret-creation"
-}
+# Secret creation module calling (temporary commented)
+#module "secret-creation" {
+#  source = "../secret-creation"
+#}
 
 # Instance launch
 resource "aws_instance" "terraform_lerning" {
@@ -9,12 +9,14 @@ resource "aws_instance" "terraform_lerning" {
   instance_type               = var.instance_type
   key_name                    = var.key_pair
   associate_public_ip_address = true
-  #user_data                  = file("${path.module}/script.sh") # old version of user data
-  user_data = templatefile("${path.module}/script.sh.tpl", {
-    secret_id = module.secret-creation.learn_secret-secret_id
-  })
+  # user data without secret creation 
+  user_data                  = file("${path.module}/script1.sh")
+  # user data that includs secret creation (temporary commented) 
+  #user_data = templatefile("${path.module}/script.sh.tpl", {
+  #  secret_id = module.secret-creation.learn_secret-secret_id # needed for secret creation (temporary commented) 
+  #})
   vpc_security_group_ids      = var.vpc_security_group_ids
-  iam_instance_profile  = module.secret-creation.iam-instance-profile-name
+#  iam_instance_profile  = module.secret-creation.iam-instance-profile-name # needed for secret creation (temporary commented)
 
  tags = {
     Terraform   = "true"
