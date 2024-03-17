c354ef0ad2531e091cc8f0d48878f4e0a9d271be
MrBazilick <96795399+MrBazilick@users.noreply.github.com>
2024-03-17 20:04:31 +0200

    import ecr

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 # output of instance public ip
-output "ec2_global_ips" {
+/*output "ec2_global_ips" {
   description = "Instance public IP: "
   value       = module.ec2_instance.ec2_ip
-}
+}*/
 
 output "account_id" {
   description = "Account ID: "
